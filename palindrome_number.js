57cab2fd1cdef9b02b508dc52972208b821a4689
akashjadhav1234 <akashjadhav20032001@gmail.com>
2023-02-16 18:06:04 +0530

    pallindrome number check

---
+++ palindrome_number.js
@@ -0,0 +1,13 @@
+let str ="naman;
+
+let str="";
+for(let i=str.length;i>=0;i++){
+   name=name+str[i];
+}
+
+if(name==str){
+	console.log("palindrome");
+}
+else{
+	console.log("Not a palindrome");
+}
